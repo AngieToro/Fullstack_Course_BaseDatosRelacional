24ceb5bb6ac1ec1933bb7def4837423611ce303f
Angelica Toro <angelicatoro88@gmail.com>
2025-11-14 17:35:53 -0300

    Se agrego la gestion de usuarios (buscar, crear y login)

--- blog_backend/util/config.js
+++ blog_backend/util/config.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 module.exports = {
     PORT: process.env.PORT || 3001,
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    SECRET: process.env.SECRET
 }
